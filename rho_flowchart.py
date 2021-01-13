9599d4c0d8e12d93002ba69be1d224c799214567
Jesse Bluestein <jessebluestein@gmail.com>
2021-01-12 22:23:13 -0800

    edges

--- rho_flowchart.py
+++ rho_flowchart.py
@@ -10,16 +10,16 @@ if __name__ == '__main__':
     # long conditions that i don't want to type multiple times
     # note - need to manually set line break locations
     cond1 = 'Each story resisting more than 35% of the base shear in\n'
-    cond1 += 'the direction of interest shall complies with Table 12.3-3.'
+    cond1 += 'the direction of interest complies with Table 12.3-3.'
 
-    cond2 = 'Structures are regular in plan at all levels provided\n'
-    cond2 += 'that the seismic force-resisting systems consist of at\n'
-    cond2 += 'least two bays of seismic force-resisting perimeter framing\n'
-    cond2 += 'on each side of the structure in each orthogonal direction at\n'
-    cond2 += 'each story resisting more than 35% of the base shear. The number\n'
-    cond2 += 'of bays for a shear wall shall be calculated as the length of shear\n'
-    cond2 += 'wall divided by the story height or two times the length of shear\n'
-    cond2 += 'wall divided by the story height, hsx, for light-frame construction.'
+    cond2 = 'None of the Horizontal Irregularities of Table 12.3-1 apply.\n'
+    cond2 += 'Seismic force-resisting systems consist of at least two bays\n'
+    cond2 += 'of seismic force-resisting perimeter framing on each side of\n'
+    cond2 += 'the structure in each orthogonal direction at each story resisting\n'
+    cond2 += 'more than 35% of the base shear. (The number of bays for a shear\n'
+    cond2 += 'wall shall be calculated as the length of shear wall divided by\n'
+    cond2 += 'the story height or two times the length of shear wall divided\n'
+    cond2 += ' by the story height, hsx, for light-frame construction.)'
 
     # create file
     g = Digraph('G', filename=fileName)
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     g.node('Flexible diaphragm', shape = shape, color = color, style = style, fillcolor = fillcolor)
     g.node('Rigid diaphragm', shape = shape, color = color, style = style, fillcolor = fillcolor)
     g.node('Extreme torsional irregularity', shape = shape, color = color, style = style, fillcolor = fillcolor)
-    g.node('Not extreme torsional irregularity', shape = shape, color = color, style = style, fillcolor = fillcolor)
+    g.node('No extreme torsional irregularity', shape = shape, color = color, style = style, fillcolor = fillcolor)
     g.node(cond1, shape = shape, color = color, style = style, fillcolor = fillcolor)
     g.node(cond2, shape = shape, color = color, style = style, fillcolor = fillcolor)
 
@@ -50,15 +50,14 @@ if __name__ == '__main__':
     g.edge('SDC D, E or F', 'Flexible diaphragm', splines='false')
     g.edge('SDC D, E or F', 'Rigid diaphragm', splines='false')
     g.edge('Rigid diaphragm', 'Extreme torsional irregularity', splines='false')
-    g.edge('Rigid diaphragm', 'Not extreme torsional irregularity', splines='false')
-    g.edge('Not extreme torsional irregularity', cond1, splines='false')
-    g.edge(cond1, cond2, label='not satisfied', splines='false')
+    g.edge('Rigid diaphragm', 'No extreme torsional irregularity', splines='false')
+    g.edge('No extreme torsional irregularity', cond1, splines='false')
+    g.edge(cond1, cond2, label = 'not satisfied', splines='false')
     g.edge('Extreme torsional irregularity', 'ρ = 1.3', splines='false')
     g.edge('Flexible diaphragm', cond1, splines='false')
-    g.edge('Flexible diaphragm', cond2, splines='false')
-    g.edge(cond1, 'ρ = 1.0', label='satisfied', splines='false')
-    g.edge(cond2, 'ρ = 1.0', label='satisfied', splines='false')
-    g.edge(cond2, 'ρ = 1.3', label='not satisfied', splines='false')
+    g.edge(cond1, 'ρ = 1.0', label = 'satisfied', splines='false')
+    g.edge(cond2, 'ρ = 1.0', label = 'satisfied', splines='false')
+    g.edge(cond2, 'ρ = 1.3', label = 'not satisfied', splines='false')
 
     # view and save flowchart
     g.view()
